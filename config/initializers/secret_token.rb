94e8c28e2efd978cec91b0d2247bc85500bffa09
Miguel Angel <elmendalerenda@gmail.com>
2013-02-21 12:04:02 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buzonciudadano::Application.config.secret_token = '65130569d2d7099f80ed1a7e1572737c2004a14df826eb9ba54b3e476afbb1a2c9165c67c110f0ca74ff78930f625395917fc2e058f79f2e627f4e284e3d2196'
