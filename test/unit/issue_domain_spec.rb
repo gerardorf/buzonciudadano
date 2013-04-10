79df738563778de2e8161126a8958c9de8dc6b6a
Pedro Gimenez <code@pedrogimenez.me>
2013-04-10 12:37:13 +0200

    refactor

--- test/unit/issue_domain_spec.rb
+++ test/unit/issue_domain_spec.rb
@@ -3,22 +3,22 @@ require_relative '../../app/models/issue'
 require_relative '../../app/models/issue_domain'
 
 describe 'IssueDomain' do
-  it 'creates an issue' do
-    domain = IssueDomain.new
-    repo = double('repo')
-    domain.repository = repo
-    repo.stub(:save)
+  before :each do
+    @domain = IssueDomain.new
+    @repo = double('repo')
+    @domain.repository = @repo
+    @repo.stub(:save)
+  end
 
-    issue = domain.create({:text => 'a text'})
+  it 'creates an issue' do
+    issue = @domain.create({:text => 'a text'})
+
     issue.to_hash[:identity].should_not be_nil
   end
 
   it 'saves an issue' do
-    domain = IssueDomain.new
-    repo = double('repo')
-    domain.repository = repo
-    repo.should_receive(:save)
+    @repo.should_receive(:save)
 
-    issue = domain.create({:text => 'a text'})
+    issue = @domain.create({:text => 'a text'})
   end
 end
